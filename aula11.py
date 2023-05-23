73c1e26eacb780a1368448a00c9dda6f53c9b2d4
Antônio Souza <antonioaugusto1913@gmail.com>
2023-05-23 11:59:39 -0300

    aulas 11 e 12

---
+++ aula11.py
@@ -0,0 +1,10 @@
+# PRECEDENCIA DE EXPRESSOES NUMERICAS
+
+# 1 - (n + n ) parenteses
+# 2 - **
+# 3 - * /, // %
+# 4 - + -
+
+conta_1 = 1 + 1 ** 5 + 5
+conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
---
+++ aula12.py
@@ -0,0 +1,7 @@
+nome = 'Antônio'
+altura = 1.75
+peso = 55
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura, pesando', peso, 'kg', 'e seu IMC é', imc)
+
