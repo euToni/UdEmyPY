edc4faeba4c289e899adc64e57bb6f536471b87b
Antônio Souza <antonioaugusto1913@gmail.com>
2023-05-23 12:45:33 -0300

    aulas 13 e 14

---
+++ aula13.py
@@ -0,0 +1,9 @@
+# FORMATAÇÃO DE STR
+
+nome = 'Antônio'
+altura = 1.75
+peso = 55
+imc = peso / (altura * altura)
+
+print(f'{nome} tem {altura:.2f} de altura e pesa {peso}, tendo assim um IMC de {imc:.3f} ')
+
---
+++ aula14.py
@@ -0,0 +1,8 @@
+a = '0.7'
+b = '0.4'
+c = 1.1
+formato = ''
+
+print(formato)
+
+print('O valor de a é {0}, o de b é igual a {2} e consequentemente c = {1}'.format(a, b, c))
