2bd21498d79ef105ea2ae28620109caa340a6977
Antônio Souza <antonioaugusto1913@gmail.com>
2023-05-29 12:07:25 -0300

    aula 15 e 16

---
+++ aula15.py
@@ -0,0 +1,11 @@
+# nome = input('Qual é o seu nome? ')
+# print('Olá {}, bom dia!!'.format(nome))
+
+n1 = input('Digite um número: ')
+n2 = input('Digite outro número: ')
+
+int_n1 = int(n1)
+int_n2 = int(n2)
+
+print(f'A soma dos números é: {int_n1 + int_n2}')
+
---
+++ aula16.py
@@ -0,0 +1,8 @@
+cond = input('Você quer "entrar" ou "sair"? ')
+
+if cond == 'entrar':
+    print('Você entrou!')
+elif cond == 'sair':
+    print('Você saiu!')
+else:
+    print('Digite: "entrar" para entrar, ou "sair" para sair.')
