5c24680710cef2cc334675dae8afeb68dce44746
Antônio Souza <antonioaugusto1913@gmail.com>
2023-05-31 11:03:42 -0300

    aulas 17, 18

---
+++ aula17.py
@@ -0,0 +1,9 @@
+cond = False
+
+if cond:
+    print('Este é o código do if')
+else:
+    print('oi')
+
+if 10==10:
+    print('10 é igual a 10')
---
+++ aula18.py
@@ -0,0 +1,15 @@
+cond1 = False
+cond2 = False
+cond3 = False
+cond4 = False
+
+if cond1:
+    print('codigo p cond1')
+elif cond2:
+    print('cod p cond2')
+elif cond3:
+    print('cod p cond3')
+elif cond4:
+    print('cod p cond4')
+else:
+    print('nenhuma cond foi satisfeita')
