228c40e58c38a0561523fd4e47e78da54ae41ff1
Antônio Souza <antonioaugusto1913@gmail.com>
2023-07-19 10:15:09 -0300

    commit

--- aulas 36 a 50/aula48-4.py
+++ aulas 36 a 50/aula48-4.py
@@ -3,3 +3,9 @@ Cuidados com dados mutáveis
 = - copiando o valor (imutáveis)
 = - aponta para o mesmo valor na memória (mutável)
 '''
+
+lista_a = ['luiz', 'maria']
+lista_b = lista_a
+
+lista_a[0] = 'qlqr coisa'
+print(lista_b)
