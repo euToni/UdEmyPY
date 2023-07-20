0570cfe4ac1e4af5a7558d02a425b54de4ec9b28
Antônio Souza <antonioaugusto1913@gmail.com>
2023-07-20 15:53:56 -0300

    pasta aula 51 a 65

---
+++ aulas 51 a 65/aula51.py
@@ -0,0 +1,9 @@
+'''
+Introdução ao desempacotamento
+'''
+
+# _, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
+# print(nome, resto)
+
+*começo, nome = ['Maria', 'Helena', 'Luiz', 1, 2, 5, 6, 7, 8, 9, 90, 34, 34, 34, 34, 34,34,34 ,4, 'ULTIMO']
+print(nome)
